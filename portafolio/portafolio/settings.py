09672e3c917e7b110d0ba6c1022fc5806e6966de
yurilealjaque <yurilealjaque2014@gmail.com>
2026-03-09 15:56:53 -0300

    se agrego el CV pdf para descarga

--- portafolio/portafolio/settings.py
+++ portafolio/portafolio/settings.py
@@ -34,7 +34,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=True)
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
